a237502f799a77e0a3bc8f597a21a102b73010ba
Dima Hubenia <dima96gubenya@gmail.com>
2024-03-28 17:54:43 +0200

    wallpapper ui

---
+++ ios-mod-gacha-1/IMGR__Flow/IMGR_TabBarScreens/ChildViews/CardViewWallpapper.swift
@@ -0,0 +1,93 @@
+//
+//  CardViewWallpapper.swift
+//  ios-mod-gacha-1
+//
+//  Created by Дима Губеня on 28.03.2024.
+//
+
+import SwiftUI
+
+struct CardViewWallpapper: View {
+    @State private var showExtraButtons = false
+    let text: String
+    let imageURL: String
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            Color.whiteLight
+            VStack {
+                RemoteImage(
+                    url: imageURL,
+                    size: .init(width: 0, height: isIPad ? 452 : 226),
+                    image: .constant(nil),
+                    cornerRadius: isIPad ? 40 : 20
+                )
+//                .background(.whiteLight)
+//                .IMGR_cornerRadius_IMGR(20, corners: .allCorners)
+
+                
+                Text("Tittle")
+                    .kerning(0.5)
+                    .IMGR_iosDeviceTypeFont(font: .init(name: .comfortaa, style: .medium, iPhoneSize: 16, iPadSize: 32))
+                    .foregroundColor(.black)
+                    .IMGR_iosDeviceTypePadding(edge: .top, iOSPadding: 6, iPadPadding: 12)
+                   
+            }
+            
+            buttonsWhenMenuOpen
+                .IMGR_iosDeviceTypePadding(edge: [.vertical, .trailing], iOSPadding: 2, iPadPadding: 4)
+        }
+        .IMGR_iosDeviceTypePadding(edge: .all, iOSPadding: 8, iPadPadding: 16)
+
+    }
+}
+
+extension CardViewCollections {
+    var buttonsWhenMenuOpen: some View {
+        VStack(spacing: 4) {
+            Button {
+                withAnimation {
+                    showExtraButtons.toggle()
+                }
+            } label: {
+                Image(.showButton)
+            }
+            
+            if showExtraButtons {
+                
+                Button(action: {
+                    //
+                }) {
+                    Image(.starButton)
+                }
+                
+                Button(action: {
+                    //
+                }) {
+                    Image(.downloadButton)
+                }
+                
+                Button(action: {
+                    //
+                }) {
+                    Image(.screenButton)
+                }
+                
+                Button(action: {
+                    //
+                }) {
+                    Image(.shareButton)
+                }
+                
+            }
+            
+        }
+        .IMGR_iosDeviceTypePadding(edge: .all, iOSPadding: 8, iPadPadding: 16)
+        .background(showExtraButtons ? .blureButton : .clear)
+        .IMGR_cornerRadius_IMGR(isIPad ? 120 : 60, corners: .allCorners)
+    }
+}
+
+#Preview {
+    CardViewWallpapper()
+}
---
+++ ios-mod-gacha-1/IMGR__Flow/IMGR_WallpapperView.swift
@@ -0,0 +1,81 @@
+//
+//  IMGR_WallpapperView.swift
+//  ios-mod-gacha-1
+//
+//  Created by Дима Губеня on 28.03.2024.
+//
+
+import SwiftUI
+import FlowStacks
+
+struct IMGR_WallpapperView: View {
+    @EnvironmentObject var navigator: FlowNavigator<IMGR_MainRoute>
+    @State private var searchText = ""
+    @State private var isModalShow = false
+    
+    @FetchRequest<IMGR_CollectionsCD>(fetchRequest: .IMGR_collections())
+    private var collections
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        IMGR_ZStackWithBackground {
+            VStack {
+                navBarViewCollections
+                IMGR_CustomSearchBar_Ext(searchText: $searchText)
+                    .IMGR_iosDeviceTypePadding(edge: .top, iOSPadding: 20, iPadPadding: 40)
+                collectionsList
+                    .IMGR_iosDeviceTypePadding(edge: .all, iOSPadding: 20, iPadPadding: 40)
+            }
+        }
+    }
+    
+    var collectionsList: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(collections) { collection in
+                    CardViewCollections(text: "/\(collection.name ?? "")", imageURL: "/\(collection.image ?? "")")
+                }
+                .background(.white)
+                .opacity(0.8)
+                .IMGR_cornerRadius_IMGR(isIPad ? 40 : 20, corners: .allCorners)
+                .overlay {
+                    RoundedRectangle(cornerRadius: isIPad ? 40 : 20)
+                        .stroke(.white, lineWidth: isIPad ? 2 : 1)
+                }
+            }
+        }
+    }
+}
+
+//MARK: Child Views
+private extension IMGR_CollectionsView {
+    
+    var navBarViewCollections: some View {
+        Text("Collections")
+            .kerning(0.4)
+            .frame(maxWidth: .infinity)
+            .IMGR_iosDeviceTypeFont(font: .init(name: .comfortaa, style: .medium, iPhoneSize: 28, iPadSize: 56))
+            .foregroundColor(.black)
+            .IMGR_iosDeviceTypePadding(edge: .top, iOSPadding: 19, iPadPadding: 38)
+            .overlay(alignment: .trailing) {
+                Button {
+                    
+                } label: {
+                    Image(.property1Filters)
+                        .resizable()
+                        .frame(width: isIPad ? 48 : 24)
+                        .frame(height: isIPad ? 48 : 24)
+                        .IMGR_iosDeviceTypePadding(edge: .trailing, iOSPadding: 22, iPadPadding: 44)
+                        .IMGR_iosDeviceTypePadding(edge: .top, iOSPadding: 22, iPadPadding: 44)
+                }
+            }
+    }
+}
+
+#Preview {
+    IMGR_WallpapperView()
+}
